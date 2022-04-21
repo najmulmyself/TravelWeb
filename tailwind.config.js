a2764f2484d8669096b21f165f86e2de42a4dbb3
najmulmyself <najmul.myself@gmail.com>
2022-04-22 00:08:31 +0600

    Project init , error with tailwind.config.js , fixed with content(**/*.html)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["**/*.html"], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
